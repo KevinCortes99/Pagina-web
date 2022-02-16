22ce9fdf745224678ac7696e0eaa29c09951964c
Kevin Cortes <kazc.99@gmail.com>
2022-02-16 19:02:22 +0100

    pagina 2.0

---
+++ prueba.php
@@ -0,0 +1,6 @@
+<?php
+include("vendor/autoload.php");
+
+$desplegar=array("I","Love","You");
+d($desplegar);
+//var_dump ($amigos);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'prueba/kevin',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kint-php/kint' => array(
+            'pretty_version' => '4.1.1',
+            'version' => '4.1.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../kint-php/kint',
+            'aliases' => array(),
+            'reference' => 'e64b939f9ceb9620abd982e2a66a3289fcf4e837',
+            'dev_requirement' => false,
+        ),
+        'prueba/kevin' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
